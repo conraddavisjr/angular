9c5fe467c3baa9ce914d67c09ca9b6ee378cbec6
Conrad Davis <conraddavis@google.com>
2018-07-16 02:04:50 -0700

    added an offline mode

--- 1.x/todo.js
+++ 1.x/todo.js
@@ -1,36 +1,71 @@
-angular.module('todoApp', [])
-  .controller('TodoCtrl', todoCtrl);
+(function() {
+
+	var app = angular.module('todoApp', []);
 
 
-function todoCtrl() {
-	console.log('called', todoCtrl)
-	var todoList = this;
+	function todoCtrl() {
+		var todoList = this;
 
-  todoList.todos = [
-		{text: 'Learn Angular', done: false},
-		{text: 'something else', done: false},
-	];
+	  todoList.todos = [
+			{text: 'Learn Angular', done: false},
+			{text: 'something else', done: false},
+		];
 
-	todoList.getTotalTodos = function () {
-		return todoList.todos.length;
+		todoList.getTotalTodos = function () {
+			return todoList.todos.length;
+		}
+
+		todoList.addTodo = function () {
+			todoList.todos.push({text: todoList.formTodoText, done: false});
+			todoList.formTodoText = '';
+		}
+
+		todoList.add = function () {
+			return todoList.val1 + todoList.val2;
+		}
+
+		todoList.clearCompleted = function () {
+			todoList.todos = todoList.todos.filter((todo) => {
+				return !todo.done;
+			})
+		}
 	}
 
-	todoList.addTodo = function () {
-		todoList.todos.push({text: todoList.formTodoText, done: false});
-		todoList.formTodoText = '';
+	var MainController = function($scope, $http) {
+	  $scope.message = 'Github viewer';
+
+	  var onUserComplete = function(response) {
+	  	console.log('response: ', response);
+	    // $scope.user = response.data;
+	  }
+
+	  var onError = function(reason) {
+	  	console.log('called')
+	  	$scope.error = 'could not fetch the user';
+	  }
+
+	  var findUser = function(response) {
+	  	var userList = response.data.users;
+	  	var filteredUser = userList.filter((user) => user.name === $scope.userName);
+	  	if (filteredUser.length === 0) onError();
+	  	$scope.user = filteredUser[0];
+	  	console.log('$scope.user: ', $scope.user);
+	  }
+
+	  $scope.requestUser = function() {
+			// $http.get(`https://api.github.com/users/${$scope.userName}`)
+			// .then(onUserComplete, onError);
+
+			// offline version
+			$http.get(`/json/mock.json`)
+	      .then(findUser, onError);
+	  }
 	}
 
-	todoList.add = function () {
-		return todoList.val1 + todoList.val2;
-	}
+	app.controller('TodoCtrl', todoCtrl);
+	app.controller('MainController', MainController);
 
-	todoList.clearCompleted = function () {
-		todoList.todos = todoList.todos.filter((todo) => {
-			return !todo.done;
-		})
-	}
-}
-
+}());
 
 
 // function TodoCtrl($scope) {
