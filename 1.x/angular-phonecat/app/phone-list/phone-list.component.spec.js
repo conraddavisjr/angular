6f9785ac2525e6fee4003c824ae4a2c892d4e609
Conrad Davis <conraddavis@google.com>
2018-08-14 14:37:39 -0700

    added

---
+++ 1.x/angular-phonecat/app/phone-list/phone-list.component.spec.js
@@ -0,0 +1,16 @@
+// describe('phonesController', function () {
+
+// 	beforeEach(module('phoneCat'))
+
+// 	var $controller;
+// 	beforeEach(inject(function (_$controller_) {
+// 		$controller = _$controller_;
+// 	}))
+
+// 	it('should have the name Conrad with a capitol C', function() {
+// 		var scope = {};
+// 		$controller('phonesController', {$scope: scope})
+// 		console.log('scope: ', scope);
+// 		expect(scope.name).toBe('Conrad')
+// 	})
+// })
